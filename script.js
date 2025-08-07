ab439d27ff985830eb42035205b9f9e7e33ea9e5
ibraheem742 <ibrahimejaz280@gmail.com>
2025-08-07 11:00:23 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const buttons = document.querySelectorAll('.getStarted');
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    alert('Thank you for choosing Task Flow! 🚀');
+  });
+});
+
+
+
